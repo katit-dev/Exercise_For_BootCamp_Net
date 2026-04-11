d6d4424525551dc98912b83b6cecbb0641075bac
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-04-11 12:53:07 +0700

    add bt_buoi22_carExercise

---
+++ Car_Exercise/Models/CarVM.cs
@@ -0,0 +1,9 @@
+namespace Car_Exercise.Models
+{
+    public class CarVM
+    {
+        public string Color { get; set; }
+        public string Img { get; set; }
+        public string Icon { get; set; }
+    }
+}
