254484bcba3ebf381127b5008def04ff8edf8f2a
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-04-11 13:23:44 +0700

    remove Car_Exercise

--- Car_Exercise/Models/CarVM.cs
+++
@@ -1,9 +0,0 @@
-namespace Car_Exercise.Models
-{
-    public class CarVM
-    {
-        public string Color { get; set; }
-        public string Img { get; set; }
-        public string Icon { get; set; }
-    }
-}
