5a719f200e88e1aa4d3f815a2e3e2e04d7bbb6cf
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-04-20 13:34:32 +0700

    add bt buoi 24

---
+++ BT_buoi24_MyWallet/bt_buoi24/Models/TransactionVM.cs
@@ -0,0 +1,9 @@
+namespace bt_buoi24.Models
+{
+    public class TransactionVM
+{
+    public string Type { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime Time { get; set; }
+}
+}
